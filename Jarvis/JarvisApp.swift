f9578fb3719b509e01013222e3501a6084d68896
Sergio Daniel <me@zheref.io>
2024-09-19 00:11:20 -0500

    Initial Commit

---
+++ Jarvis/JarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisApp.swift
+//  Jarvis
+//
+//  Created by Sergio Daniel on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct JarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JarvisTests/JarvisTests.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisTests.swift
+//  JarvisTests
+//
+//  Created by Sergio Daniel on 19/09/24.
+//
+
+import Testing
+@testable import Jarvis
+
+struct JarvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
