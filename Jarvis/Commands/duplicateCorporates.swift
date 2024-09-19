3152e6497bd33a17d71e1e3961814ad897ae8cd9
Sergio Daniel <me@zheref.io>
2024-09-19 09:59:37 -0500

    Progress on duplicate corporates

--- Jarvis/Commands/duplicateCorporates.swift
+++ Jarvis/Commands/duplicateCorporates.swift
@@ -29,22 +29,70 @@ private func formatTime(fromDate date: Date) -> String {
     return formatter.string(from: date)
 }
 
+extension EKEvent {
+    var seemsCorporate: Bool {
+        calendar.title.contains("@") || calendar.source.title.contains("@")
+    }
+}
+
+private func areRepresentingSameEvent(_ event1: EKEvent, _ event2: EKEvent) -> Bool {
+    event1.title == event2.title &&
+    ((event1.startDate == event2.startDate &&
+      event1.endDate == event2.endDate) || (event1.isAllDay && event2.isAllDay))
+}
+
+extension Array where Element == EKEvent {
+    func hasDuplicate(ofEvent event: EKEvent) -> Bool {
+        contains(where: { areRepresentingSameEvent($0, event) })
+    }
+}
+
 func duplicateCorporatesCommand() -> AnyPublisher<String, Error> {
     .create { receiver in
         let store = EKEventStore()
         receiver.send("Fetching events for today...")
         
-        let events = fetchEvents(usingStore: store)
-        receiver.send("Found \(events.count) events.")
+        let allEvents = fetchEvents(usingStore: store)
         
-        for (index, event) in events.enumerated() {
+        let nonCorporateEvents = allEvents
+            .filter { $0.seemsCorporate == false }
+        let corporateEvents = allEvents
+            .filter(\.seemsCorporate)
+        receiver.send("Found \(allEvents.count) events.")
+        receiver.send("Found \(corporateEvents.count) corporate events.")
+        
+        receiver.send("Non Corporate Events: -------------------------------")
+        for (index, event) in nonCorporateEvents.enumerated() {
             if let title = event.title, let startDate = event.startDate, let endDate = event.endDate {
                 receiver
                     .send(
-                        "(\(index+1)) \(title) \(formatTime(fromDate: startDate)) - \(formatTime(fromDate: endDate)) from \(event.calendar.title)"
+                        "(\(index+1)) \(title) \(formatTime(fromDate: startDate)) - \(formatTime(fromDate: endDate)) from \(event.calendar.title) in \(event.calendar.source.title)"
                     )
             }
-            
+        }
+        
+        receiver.send("Corporate Events: -----------------------------------")
+        for (index, event) in corporateEvents.enumerated() {
+            if let title = event.title, let startDate = event.startDate, let endDate = event.endDate {
+                receiver
+                    .send(
+                        "(\(index+1)) \(title) \(formatTime(fromDate: startDate)) - \(formatTime(fromDate: endDate)) from \(event.calendar.title) in \(event.calendar.source.title)"
+                    )
+            }
+        }
+        
+        let candidatesToDuplicate = corporateEvents.filter {
+            nonCorporateEvents.hasDuplicate(ofEvent: $0) == false
+        }
+        
+        receiver.send("Candidates to Duplicate: ------------------------------")
+        for (index, event) in candidatesToDuplicate.enumerated() {
+            if let title = event.title, let startDate = event.startDate, let endDate = event.endDate {
+                receiver
+                    .send(
+                        "(\(index+1)) \(title) \(formatTime(fromDate: startDate)) - \(formatTime(fromDate: endDate)) from \(event.calendar.title) in \(event.calendar.source.title)"
+                    )
+            }
         }
         
         receiver.send(completion: .finished)
