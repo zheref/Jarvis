114d717143ea605a17de80cc540b5f9a2e172747
Sergio Daniel <me@zheref.io>
2024-10-02 14:03:42 -0500

    First attempt to request permissions

--- jarvis-cli/Main.swift
+++ jarvis-cli/Main.swift
@@ -7,21 +7,45 @@
 
 import Combine
 import Foundation
+import JarvisLib
 
 @main
 struct JarvisCLI {
-    var disposeBag = Set<AnyCancellable>()
+    static var disposeBag = Set<AnyCancellable>()
+    
+    static var currentTimestamp: String {
+        let currentDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter.string(from: currentDate)
+    }
     
     static func main() {
+        print("Hello Jarvis CLI with Own Framework!")
 //        if CommandLine.arguments.contains("--enable-permissions") {
 //            
 //        }
         
-//        let enablePermissionsCF = enablePermissionsCommand(
-//            CommandFlowConfig(performanceClass: .simulation)
-//        )
+        let enablePermissionsCF = enablePermissionsCommand(
+            CommandFlowConfig(performanceClass: .simulation,
+                              shouldSaveLogs: true)
+        )
         
-        print("Hello Jarvis CLI with Own Framework!")
+        enablePermissionsCF
+            .subscribe(on: RunLoop.main)
+            .catch {
+                Just(
+                    "ERROR: \($0.localizedDescription)"
+                )
+            }
+            .print()
+            .sink { _ in print("[\(currentTimestamp)] Completed.") }
+            receiveValue: { print("[\(currentTimestamp)] \($0)") }
+            .store(in: &disposeBag)
         
+        
+        print("Staying in the loop...")
+        RunLoop.main.run(until: .distantFuture)
+        print("Ended the loop.")
     }
 }
