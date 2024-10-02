b7e7dd6332ef86864d1928646dd11f8f076c49ad
Sergio Daniel <me@zheref.io>
2024-10-02 13:04:48 -0500

    Change to full extended OO CLI

---
+++ jarvis-cli/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  Jarvis
+//
+//  Created by Sergio Daniel on 10/2/24.
+//
+
+import Foundation
+
+@main
+struct JarvisCLI {
+    static func main() {
+        print("Hello Jarvis CLI!")
+    }
+}
